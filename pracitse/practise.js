9596b757ebd150620d6f25e9d48428cbabbfb146
shivam7242 <96744231+shivam7242@users.noreply.github.com>
2022-07-09 12:49:24 +0530

    check

---
+++ pracitse/practise.js
@@ -0,0 +1,130 @@
+console.log(
+    "this is shviam's work in which he is storing all the neccessary information inside it"
+);
+var a = 20;
+var b = 30;
+document.write(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+var s1 = "  sfdgasjkhfddhfs";
+var s2 = "   sfdgasjkhfddhfs";
+document.write(s1 + s2);
+document.write(s1);
+document.write(s1);
+var u = 100;
+var v = 10;
+
+document.write("the value of u is ", u);
+document.write("the value of v is ", v);
+document.write(u + v);
+document.write(u - v);
+document.write(u * v);
+document.write(u / v);
+console.log(u + v);
+console.log(u - v);
+console.log(u * v);
+console.log(u / v);
+var age = 3;
+if (age > 30) {
+    document.write("bacche nhi  rhe");
+} else if (age > 25) {
+    document.write("bacche nhi  rhe");
+} else if (age > 20) {
+    document.write("bacche young hai  nhi  rhe");
+} else if (age > 17) {
+    document.write("bacche nhi age  > 17   rhe");
+} else if (age > 15) {
+    document.write("bacche nhi  age > 15 rhe");
+} else {
+    document.write(
+        "baccha hai tu sasdfjhvaksdgagsacdagaksakxcbnbckzhant rah hamesa"
+    );
+}
+var arr = [1, 2, 3, 4, 5, 6, 7];
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+}
+let myarr = ["shivam", "raam", "shyam", 23, null];
+console.log(myarr.length);
+console.log(myarr.length);
+console.log(myarr);
+for (let index = 0; index < myarr.length; index++) {
+    console.log(myarr[index]);
+}
+myarr.pop("23");
+myarr.pop();
+myarr.pop();
+myarr.pop();
+
+myarr.push("manav", " sfdvc", "asdfgb");
+console.log(myarr);
+
+myarr.shift();
+console.log(myarr);
+// myarr.shift();
+// myarr.shift();
+// myarr.shift();
+
+console.log(myarr);
+myarr.unshift("aasdgf", "asdfdsdfgfe", "wert");
+console.log(myarr);
+// myarr.push("poiuy");
+console.log(myarr);
+console.log(myarr);
+// console.log(myarr);
+myarr.toString();
+console.log(myarr);
+let d = [1, 2, 3, 5, , 8, , 54, 12];
+console.log(d);
+
+d.sort();
+console.log(d);
+let str = "thiis is my stasdfg jhgefd";
+console.log(str.length);
+console.log(str.indexOf(" "));
+console.log(str.lastIndexOf(" "));
+console.log(str.slice(2, 5));
+console.log(str.slice(10, 15));
+// console.log(str.reverse())
+console.log(str);
+
+str.replace("thiis", " raman");
+str = str.replace("thiis", " ramanasdfghkjhgfd");
+console.log(str);
+str = str.replace("thiis", " ramanasdfghkjhgfd");
+str = str.replace("ramanasdfghkjhgfd", "poiuytre ");
+console.log(str);
+console.log("shivam pratap is a good boy and handsome also");
+let shvia = new Array();
+shiva = [1, 2, 3, 4, 5, 6, "asdfghss", "asdfg", 52];
+console.log(shiva);
+shiva.sort();
+console.log(shiva);
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + "/" + dd + "/" + yyyy;
+console.log(today);
+let mydate = new Date();
+console.log(mydate);
+console.log(mydate.getDate());
+console.log(mydate.getTime());
+console.log(mydate.getHours());
+console.log(mydate.getUTCDay());
+console.log(mydate.getMonth());
+console.log(mydate);
+console.log(mydate);
+console.log(mydate);
+let myarr = ["shivam", "raam", "shyam", 23, null];
+console.log(myarr.length);
+console.log(myarr.length);
+console.log(myarr);
+for (let index = 0; index < myarr.length; index++) {
+    console.log(myarr[index]);
+}
---
+++ pracitse/practise1.js
@@ -0,0 +1,14 @@
+document.write("dskh    shfk hja jh fakj");
+document.write("        dsk h sh fk hjh jf hs hjj");
+document.write("d skhs   hd b  ajhkj");
+console.log("hxgsdhc");
+console.log("hxsdfsvdesekjjdfhse");
+console.log("12345678f9cfssdfhse");
+console.log("1234569eeedsejdfhse");
+console.log({ shivam: "shiva", marks: 45 });
+console.table({
+    shivam: "shiva",
+    marks: 45,
+    age: "sejhsgsd  V v c  '/fs",
+    sex: "make",
+});
